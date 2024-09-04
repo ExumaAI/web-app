ce27f1374b3e3e7147b60fb2564a4ac274a1e4d7
Bilal Ch <1557123+TheRed002@users.noreply.github.com>
2024-09-04 23:31:02 +0500

    fix: enabled laravel-installer middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \RachidLaasri\LaravelInstaller\Middleware\ApplicationCheckLicense::class,
-            // \RachidLaasri\LaravelInstaller\Middleware\ApplicationStatus::class,
+            \RachidLaasri\LaravelInstaller\Middleware\ApplicationStatus::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\UpdateUserActivity::class,
